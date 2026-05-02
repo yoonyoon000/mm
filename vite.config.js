b8db8e97dc66e6b1bb5138b4a49d156465616c61
yoonyoon000 <yoonyoon000@users.noreply.github.com>
2026-05-03 08:10:43 +0900

    Improve desktop CSS compatibility

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: './',
   build: {
+    cssTarget: 'chrome80',
     rollupOptions: {
       input: 'build.html',
       output: {
