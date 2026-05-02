48764628f328ab1e0e501640dfd418dd40297727
yoonyoon000 <yoonyoon000@users.noreply.github.com>
2026-05-03 08:43:43 +0900

    Use standard Vite Pages build

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: './',
-  build: {
-    rollupOptions: {
-      input: 'build.html',
-      output: {
-        entryFileNames: 'assets/app-mobile.js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: (assetInfo) => (
-          assetInfo.name && assetInfo.name.endsWith('.css')
-            ? 'assets/app-mobile.css'
-            : 'assets/[name][extname]'
-        ),
-      },
-    },
-  },
   plugins: [react()],
 });
